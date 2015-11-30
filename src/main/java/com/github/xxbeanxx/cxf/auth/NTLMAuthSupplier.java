1807b5540872c1d3fe912c26974a52f14600bfc9
Greg Baker <greg.baker.bsc@gmail.com>
2015-11-29 21:44:15 -0330

    possibly working NTLM

--- src/main/java/com/github/xxbeanxx/cxf/auth/NTLMAuthSupplier.java
+++ src/main/java/com/github/xxbeanxx/cxf/auth/NTLMAuthSupplier.java
@@ -5,18 +5,21 @@ import java.net.URI;
 import org.apache.cxf.configuration.security.AuthorizationPolicy;
 import org.apache.cxf.message.Message;
 import org.apache.cxf.transport.http.auth.HttpAuthSupplier;
+import org.apache.http.impl.auth.NTLMEngine;
+import org.apache.http.impl.auth.NTLMScheme;
 
 /**
  * @author Greg Baker
  */
 public class NTLMAuthSupplier implements HttpAuthSupplier {
 
+	NTLMScheme ntlmScheme = new NTLMScheme();
+	
 	public boolean requiresRequestCaching() {
 		return false;
 	}
 
 	public String getAuthorization(AuthorizationPolicy authPolicy, URI url, Message message, String fullHeader) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
